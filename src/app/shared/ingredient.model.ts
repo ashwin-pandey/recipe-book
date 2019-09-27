fcef37746f816447e7a69320140c230587d0af4b
ashwin-pandey <ashwin.pandey11197@gmail.com>
2019-09-27 14:50:11 +0530

    working

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,12 @@
+export class Ingredient {
+	// public name: string;
+	// public amount: number;
+
+	// constructor(name: string, amount: number) {
+	// 	this.name = name;
+	// 	this.amount = amount;
+	// }
+
+	// Does the same thing as implemented above
+	constructor(public name: string, public amount: number) {}
+}
